de672b4e77f89c6060f9baff7ebe3ccba264bfdb
Shawnade <deshawnagalloway@yahoo.com>
2025-05-18 21:40:40 -0400

    added show route

--- controllers/games.js
+++ controllers/games.js
@@ -8,18 +8,36 @@ const router = express.Router();
 
 router.post('/', verifyToken, async (req, res) => {
     const authorId = req.user._id;
-    console.log(req.body)
     try {
-        // console.log(req.user._id);
         let authorPackage = req.body
         authorPackage.author = authorId
         const game = await Game.create(req.body);
         console.log(req.body);
-        // game._doc.author = req.user;
+        game._doc.author = req.user;
         res.status(201).json(game);
     } catch (err) {
         res.status(500).json({ err: err.message });
     }
+});
+
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const games = await Game.find({})
+      .populate("author")
+      .sort({ createdAt: "desc" });
+    res.status(200).json(games);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+router.get("/:gameId", verifyToken, async (req, res) => {
+  try {
+    const game = await Game.findById(req.params.gameId).populate("author");
+    res.status(200).json(game);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
 });
 
 module.exports = router;
